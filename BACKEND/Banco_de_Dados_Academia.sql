2393827883311ab23f79246bdbf202e06082bc96
Diogo Ribeiro <diogosribeiro5@gmail.com>
2026-04-16 22:22:29 -0300

    Add files via upload

---
+++ BACKEND/Banco_de_Dados_Academia.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE ACADEMIA;
+
+USE ACADEMIA;
+
+CREATE TABLE planos (
+	id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    modalidade VARCHAR(80) NOT NULL,
+    valor_mensal DECIMAL(10,2) NOT NULL,
+    ativo BIT NOT NULL DEFAULT 1
+);
+    
+CREATE TABLE alunos (
+	id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(120) NOT NULL,
+    email VARCHAR(120) NOT NULL UNIQUE,
+    idade INT NOT NULL,
+	telefone VARCHAR (20) NOT NULL,
+    situacao VARCHAR (20) NOT NULL,
+    data_cadastro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    plano_id BIGINT NOT NULL,
+    CONSTRAINT fk_alunos_planos
+    FOREIGN KEY (plano_id) REFERENCES planos(id)
+    );
+    
+    
